e7e53fd7928b65f49a6092089ecb39c6aca9f7e7
FATEH BEDDIAF <kentounmax@gmail.com>
2023-05-29 23:20:49 +0100

    end listing details

---
+++ components/Inputs/Calendar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { DateRange, Range, RangeKeyDict } from "react-date-range";
+
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+interface DatePickerProps {
+  value: Range;
+  onChange: (value: RangeKeyDict) => void;
+  disabledDates?: Date[];
+}
+
+const DatePicker = ({ value, onChange, disabledDates }: DatePickerProps) => {
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  );
+};
+
+export default DatePicker;
